e6c8547a9e2d0218574f7ceda561bba68fe22397
kristenmills <kristen@kristen-mills.com>
2014-08-20 20:24:18 -0700

    Updated cli

--- lib/ignoring/cli.rb
+++ lib/ignoring/cli.rb
@@ -5,28 +5,37 @@ module Ignoring
   class CLI < Thor
 
     desc "create", "creates a new gitignore if one doesn't exist in the directory"
+    option :global, type: :boolean, desc: "whether to use global gitignore", aliases: :g
     def create
+      # TODO
     end
 
     desc "add [ITEM...]", "Adds a new item to the gitignore if it isn't already added"
+    option :global, type: :boolean, desc: "whether to use global gitignore", aliases: :g
     def add(*items)
+      # TODO
     end
 
     desc "language [LANGUAGE...]", "Adds the gitignore from github/gitignore repo to the gitignore."
+    option :global, type: :boolean, desc: "whether to use global gitignore", aliases: :g
     def language(*language)
+      # TODO
     end
 
     desc "lang [LANGUAGE...]", "Adds the gitignore from github/gitignore repo to the gitignore."
+    option :global, type: :boolean, desc: "whether to use global gitignore", aliases: :g
     alias_method :lang, :language
 
     desc "list", "lists the different languages available from github/gitignore"
     def list
+      # TODO
     end
 
     desc "show", "displays a gitignore"
-    option :global, type: :boolean, desc: "prints out global gitignore", aliases: :g
+    option :global, type: :boolean, desc: "whether to use global gitignore", aliases: :g
     option :language, desc: "displays gitignore for language from github/gitignore repo", aliases: :l
     def show
+      # TODO
     end
   end
 end
